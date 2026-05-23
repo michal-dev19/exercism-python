905d2851399f33b356478c448003304d83707780
Michal <michaelsal1904@gmail.com>
2026-05-23 18:28:15 +0100

    complete reverse_string

---
+++ reverse-string/reverse_string.py
@@ -0,0 +1,12 @@
+"""Modules serves to reverse a string."""
+
+
+def reverse(text):
+    """Reverse a string.
+
+    Args:
+        text: A string to reverse.
+
+    Returns:
+        The reversed string."""
+    return text[::-1]
